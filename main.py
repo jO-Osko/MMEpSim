8a78f683b390d8ddacfd18c04ee04300fd9b2935
jO-Osko <koprivec.filip@gmail.com>
2017-08-09 17:06:33 +0200

    Base structure

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Filip Koprivec"
+__email__ = "koprivec.fili@gmail.com"
+
+"""
+Glavni program
+"""
+
+
+def main() -> bool:
+    return True
+
+
+if __name__ == "__main__":
+    main()
