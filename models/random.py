5dbb79d1bec4711b683fcb61a32ec5b68ae84be1
Filip <koprivec.filip@gmail.com>
2017-08-13 10:57:22 +0200

    Added interfaces for random generation

---
+++ models/random.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Modul, ki poenoti funkcije naključni števil, predpiše neke vrste vmesnike(interface)
+"""
+import random
+
+from typing import List, TypeVar, Callable, Sequence
+
+__author__ = "Filip Koprivec"
+__email__ = "koprivec.filip+template@gmail.com"
+
+T = TypeVar("T")
+
+items_t = List[T]
+weights_t = Sequence[float]
+
+
+def random_select(seq: items_t, p: weights_t) -> T:
+    """
+    Funkcija, ki vrne prvi element pythonovega internega random.choices (funkcija za uteženi izbor števila).
+    :param seq: Seznam elementov med katerimi izbiramo
+    :param p: Seznam uteži za posamezni element iz seq
+    :return: Saključno (uteženo) izbran element iz seznama seq
+    """
+    return random.choices(seq, p, k=1)[0]
+
+
+def select(seq: items_t, p: weights_t, rand_choice: Callable[[items_t, weights_t], T] = random_select) -> T:
+    """
+    Funkcija, ki vrne (uteženo) naključno izbrano število izmed seq, pri tem za ibor uporabi funkcijo podano kot parameter rand_choice ali random_select, če le ta ni podana
+    :param seq: Seznam elementov med katerimi izbiramo
+    :param p: Seznam uteži za posamezni element iz seq
+    :param rand_choice: Funkcija za uteženo izbiranje elementov
+    :return: Naključno (uteženo) izbran element iz seznama seq
+    """
+    return rand_choice(seq, p)
+
+
+def select_multiple(seq: items_t, p: weights_t, n: int,
+                    rand_choice: Callable[[items_t, weights_t], T] = random_select) -> List[T]:
+    """
+    Funkcija, ki vrne seznam n (uteženo) naključno izbrano število izmed seq, pri tem za ibor uporabi funkcijo podano kot parameter rand_choice ali random_select, če le ta ni podana
+    :param seq: Seznam elementov med katerimi izbiramo
+    :param p: Seznam uteži za posamezni element iz seq
+    :param n: Število elementov v vrnjenem seznamu
+    :param rand_choice: Funkcija za uteženo izbiranje elementov
+    :return: Seznam (uteženo) naključno izbranih elementov, dolžine n
+    """
+    return [select(seq, p, rand_choice) for _ in range(n)]
